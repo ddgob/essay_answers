005ab82385006c44d3bda72846ff675fec86b96f
Daniel Barbosa <danielolibar@gmail.com>
2024-10-06 16:44:16 -0300

    test(`TestAnswerService`): add new test cases for untested methods in AnswerService
    - Added test cases for `get_best_answers_based_on_subtitle`, including edge cases:
      - Empty queries.
      - Text containing only subtitles.
    - Added tests for `get_best_subtitles` method:
      - Handle empty essays.
      - Handle essays with no subtitles and return a default "Introduction" subtitle.
    - Rename some tests to reflect the refactor to the method names in `AnswerService` made in the last few commits
    - Refactored the test for `get_best_subtitles` to include encoder functionality.
    - Improved test documentation with detailed docstrings and type annotations.

--- tests/test_answer_service.py
+++ tests/test_answer_service.py
@@ -5,11 +5,20 @@ handling queries, processing essays, and providing the most relevant
 answers.
 """
 
+import os
+import sys
 from typing import List, Dict
 
 import pytest
 
-from answer_service.answer_service import AnswerService
+from answer_service.answer_service import AnswerService, Encoder
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+project_root = os.path.abspath(os.path.join(current_dir, '..'))
+build_path = os.path.join(project_root, 'answer_service', 'sentence_embedding', 'build')
+sys.path.append(build_path)
+
+from sentence_embedding import SentenceEmbedding # type: ignore
 
 class TestAnswerService:
     """
@@ -20,13 +29,14 @@ class TestAnswerService:
     in various scenarios, including edge cases.
     """
 
-    def test_answer_questions_detailed(self) -> None:
+    def test_get_best_answers(self) -> None:
         """
-        Test answer_questions_detailed to ensure detailed answers with
+        Test get_best_answers to ensure detailed answers with
         similarity scores are returned for each query.
 
         Asserts:
-            Correct detailed answers are returned in the expected format.
+            Correct detailed answers are returned in the expected 
+            format.
         """
         essay: str = "This is the first sentence. This is the second sentence."
         queries: List[str] = [
@@ -35,7 +45,7 @@ class TestAnswerService:
         ]
 
         answer_service: AnswerService = AnswerService()
-        answers: Dict[str, tuple[str, float]] = answer_service.answer_questions_detailed(
+        answers: Dict[str, tuple[str, float]] = answer_service.get_best_answers(
             essay,
             queries
         )
@@ -44,11 +54,12 @@ class TestAnswerService:
 
     def test_answer_questions(self) -> None:
         """
-        Test answer_questions to ensure a list of answers is returned for
-        each query.
+        Test answer_questions to ensure a list of answers is returned 
+        for each query.
 
         Asserts:
-            The length of returned answers matches the number of queries.
+            The length of returned answers matches the number of 
+            queries.
         """
         essay: str = "This is the first sentence. This is the second sentence."
         queries: List[str] = [
@@ -61,10 +72,10 @@ class TestAnswerService:
 
         assert len(answers) == len(queries)
 
-    def test_get_best_subtitles(self) -> None:
+    def test_get_best_subtitles(self, encoder: Encoder) -> None:
         """
-        Test get_best_subtitles to ensure the best matching subtitles are
-        returned for the queries.
+        Test get_best_subtitles to ensure the best matching subtitles 
+        are returned for the queries.
 
         Asserts:
             Correct subtitles are matched to the queries.
@@ -72,18 +83,20 @@ class TestAnswerService:
         essay: str = "Title\nThis is the content."
         queries: List[str] = ["What is the title?"]
 
+        embedded_queries: List[SentenceEmbedding] = encoder.encode_string_list(queries)
+
         answer_service: AnswerService = AnswerService()
         best_subtitles: Dict[str, tuple[str, float]] = answer_service.get_best_subtitles(
             essay,
-            queries
+            embedded_queries
         )
 
-        assert "What is the title?" in best_subtitles
+        assert "Title" == best_subtitles["What is the title?"][0]
 
-    def test_answer_based_on_subtitle(self) -> None:
+    def test_answer_questions_based_on_subtitle(self) -> None:
         """
-        Test answer_based_on_subtitle to ensure correct answers are returned
-        based on subtitles for each query.
+        Test answer_questions_based_on_subtitle to ensure correct 
+        answers are returned based on subtitles for each query.
 
         Asserts:
             Correct answers are returned based on subtitle matching.
@@ -92,15 +105,18 @@ class TestAnswerService:
         queries: List[str] = ["What is under the title?"]
 
         answer_service: AnswerService = AnswerService()
-        answers: List[str] = answer_service.answer_based_on_subtitle(essay, queries)
+        answers: List[str] = answer_service.answer_questions_based_on_subtitle(
+            essay,
+            queries
+        )
 
         assert len(answers) == 1
         assert answers[0] == "This is the content under the title"
 
     def test_empty_essay(self) -> None:
         """
-        Test the edge case where an empty essay is provided to ensure the
-        service handles it correctly by returning no answers.
+        Test the edge case where an empty essay is provided to ensure 
+        the service handles it correctly by returning no answers.
 
         Asserts:
             No answers are returned for an empty essay.
@@ -126,7 +142,10 @@ class TestAnswerService:
         queries: List[str] = ["What is the first sentence?"]
 
         answer_service: AnswerService = AnswerService()
-        answers: List[str] = answer_service.answer_based_on_subtitle(essay, queries)
+        answers: List[str] = answer_service.answer_questions_based_on_subtitle(
+            essay,
+            queries
+        )
 
         assert len(answers) == 0
 
@@ -149,7 +168,8 @@ class TestAnswerService:
     def test_subtitle_only_text(self) -> None:
         """
         Test the case where the text contains only subtitles to ensure
-        no answers are returned if there is no content under the subtitles.
+        no answers are returned if there is no content under the 
+        subtitles.
 
         Asserts:
             No answers are returned if the text only contains subtitles.
@@ -158,7 +178,10 @@ class TestAnswerService:
         queries: List[str] = ["What is subtitle?"]
 
         answer_service: AnswerService = AnswerService()
-        answers: List[str] = answer_service.answer_based_on_subtitle(essay, queries)
+        answers: List[str] = answer_service.answer_questions_based_on_subtitle(
+            essay,
+            queries
+        )
 
         assert len(answers) == 0
 
@@ -225,6 +248,115 @@ class TestAnswerService:
             expected_answers: List[str] = file.read().splitlines()
 
         assert all(answer in answers for answer in expected_answers)
+
+    def test_get_best_answers_based_on_subtitle(self) -> None:
+        """
+        Test get_best_answers_based_on_subtitle to ensure detailed answers with 
+        similarity scores are returned for each query based on subtitle matching.
+
+        Asserts:
+            Correct detailed answers are returned in the expected format.
+        """
+
+        essay: str = "Title\nThis is the content under the title. This is a sentence."
+        queries: List[str] = ["What is under the title?"]
+
+        answer_service: AnswerService = AnswerService()
+        best_answers: Dict[str, tuple[str, float]] = (
+            answer_service.get_best_answers_based_on_subtitle(essay, queries)
+        )
+
+        assert "What is under the title?" in best_answers
+        assert best_answers["What is under the title?"][0] == "This is the content under the title"
+
+    def test_empty_queries_based_on_subtitle(self) -> None:
+        """
+        Test the edge case where an empty list of queries is provided when trying 
+        to get answers based on subtitle to ensure the service handles it correctly
+        by returning no answers.
+
+        Asserts:
+            No answers are returned for empty queries.
+        """
+
+        essay: str = "This is the first sentence. This is the second sentence."
+        queries: List[str] = []
+
+        answer_service: AnswerService = AnswerService()
+        best_answers: Dict[str, tuple[str, float]] = (
+            answer_service.get_best_answers_based_on_subtitle(essay, queries)
+        )
+
+        assert len(best_answers) == 0
+
+    def test_subtitle_only_text_for_get_best_answers_based_on_subtitle(self) -> None:
+        """
+        Test the case where the text contains only subtitles to ensure no answers 
+        are returned if there is no content under the subtitles when using 
+        get_best_answers_based_on_subtitle.
+
+        Asserts:
+            No answers are returned if the text only contains subtitles.
+        """
+
+        essay: str = "Subtitle\nSubtitle2\n"
+        queries: List[str] = ["What is subtitle?"]
+
+        answer_service: AnswerService = AnswerService()
+        best_answers: Dict[str, tuple[str, float]] = (
+            answer_service.get_best_answers_based_on_subtitle(essay, queries)
+        )
+
+        assert len(best_answers) == 0
+
+    def test_get_best_subtitles_empty_essay(self) -> None:
+        """
+        Test get_best_subtitles with an empty essay to ensure the service handles it
+        correctly by returning no subtitles.
+
+        Asserts:
+            No subtitles are returned for an empty essay.
+        """
+
+        essay: str = ""
+        queries: List[str] = ["What is the title?"]
+
+        encoder: Encoder = Encoder()
+        embedded_queries: List[SentenceEmbedding] = encoder.encode_string_list(queries)
+
+        answer_service: AnswerService = AnswerService()
+        best_subtitles: Dict[str, tuple[str, float]] = answer_service.get_best_subtitles(
+            essay,
+            embedded_queries
+        )
+
+        assert len(best_subtitles) == 0
+
+    def test_get_best_subtitles_no_subtitles_in_single_paragraph(self) -> None:
+        """
+        Test get_best_subtitles when the text contains no subtitles and 
+        a single paragraph to ensure the service handles it correctly 
+        by returning a default subtitle called "Introduction".
+
+        Asserts:
+            Subtitle "Introduction" returned if the essay has no 
+            subtitles.
+        """
+
+        essay: str = "This is the first sentence. This is the second sentence."
+        queries: List[str] = ["What is the first sentence?"]
+
+        encoder: Encoder = Encoder()
+        embedded_queries: List[SentenceEmbedding] = encoder.encode_string_list(queries)
+
+        answer_service: AnswerService = AnswerService()
+        best_subtitles: Dict[str, tuple[str, float]] = answer_service.get_best_subtitles(
+            essay,
+            embedded_queries
+        )
+
+        assert best_subtitles["What is the first sentence?"][0] == "Introduction"
+
 
 if __name__ == '__main__':
     pytest.main()
