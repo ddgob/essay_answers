3c7e04585a127de761f6e0872718ec4be74b0242
Daniel Barbosa <danielolibar@gmail.com>
2024-10-06 13:51:11 -0300

    feat(`EssayAnswersAPI`): add logging for better traceability
    - Introduce logging functionality using Python's logging module.
    - Initialize a logger in the EssayAnswersAPI class to capture important events during request handling.
    - Add logging statements in methods to log key actions, such as:
      - Validating the essay and query inputs.
      - Checking if the essay is a string and non-empty.
      - Checking if the query list is non-empty and contains only strings.
    - Log the successful completion of the POST request handling process.
    - Enhance error logging for invalid inputs, such as non-string essays and empty query lists.

--- api/essay_answers_api.py
+++ api/essay_answers_api.py
@@ -8,10 +8,15 @@ with answers for each query.
 """
 
 from typing import List, Dict, Any, Union
+import logging
+from logging import Logger
 
 from flask import Flask, request, jsonify, Response, make_response
 
 from answer_service import AnswerService
+
+format: str = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+logging.basicConfig(level=logging.INFO, format=format)
 
 class EssayAnswersAPI:
     """
@@ -26,6 +31,8 @@ class EssayAnswersAPI:
     def __init__(self) -> None:
         self.api: Flask = Flask(__name__)
         self.api.add_url_rule('/answers', view_func=self.get_answers, methods=['POST'])
+        self.logger: Logger = logging.getLogger(__name__)
+        self.logger.info("API initialized.")
 
     def get_instance(self) -> Flask:
         """
@@ -39,9 +46,14 @@ class EssayAnswersAPI:
         Checks if the essay is a string.
         """
 
+        self.logger.info("Validating if essay is a string...")
+
         if not isinstance(essay, str):
             json: Response = jsonify({"error": "Essay must be a string."})
+            self.logger.error("Essay is not a string.")
             return make_response(json, 400)
+
+        self.logger.info("Essay is a string.")
 
         return None
 
@@ -51,9 +63,14 @@ class EssayAnswersAPI:
         Checks if the essay is non-empty string.
         """
 
+        self.logger.info("Validating if essay is empty...")
+
         if not essay.strip():
             json: Response = jsonify({"error": "Essay cannot be empty."})
+            self.logger.error("Essay is empty.")
             return make_response(json, 400)
+
+        self.logger.info("Essay is not empty.")
 
         return None
 
@@ -63,6 +80,8 @@ class EssayAnswersAPI:
         Checks if the essay is a non-empty string.
         """
 
+        self.logger.info("Validating essay...")
+
         response = self.validate_string_essay(essay)
         if response:
             return response
@@ -70,6 +89,8 @@ class EssayAnswersAPI:
         response = self.validate_empty_essay(essay)
         if response:
             return response
+
+        self.logger.info("Essay is valid.")
 
         return None
 
@@ -79,9 +100,14 @@ class EssayAnswersAPI:
         Ensures that the list is not empty.
         """
 
+        self.logger.info("Validating if queries list is empty...")
+
         if not queries:
             json: Response = jsonify({"error": "Queries list cannot be empty."})
+            self.logger.error("Queries list is empty.")
             return make_response(json, 400)
+
+        self.logger.info("Queries list is not empty.")
 
         return None
 
@@ -91,9 +117,14 @@ class EssayAnswersAPI:
         Ensures that the list contains only strings.
         """
 
+        self.logger.info("Validating if queries are strings...")
+
         if not all(isinstance(query, str) for query in queries):
             json: Response = jsonify({"error": "Queries must be a list of strings."})
+            self.logger.error("Queries are not strings.")
             return make_response(json, 400)
+
+        self.logger.info("Queries are strings.")
 
         return None
 
@@ -102,6 +133,9 @@ class EssayAnswersAPI:
         Validates the queries list.
         Ensures that the list is not empty and contains only strings.
         """
+
+        self.logger.info("Validating queries...")
+
         response = self.validate_empty_queries(queries)
         if response:
             return response
@@ -109,6 +143,8 @@ class EssayAnswersAPI:
         response = self.validate_string_queries(queries)
         if response:
             return response
+
+        self.logger.info("Queries are valid.")
 
         return None
 
@@ -142,6 +178,8 @@ class EssayAnswersAPI:
             }
         """
 
+        self.logger.info("Processing POST request to /answers...")
+
         data: Dict[str, Any] = request.get_json()
         essay: str = data.get('essay', '')
         queries: List[str] = data.get('queries', [])
@@ -156,5 +194,7 @@ class EssayAnswersAPI:
 
         answer_service: AnswerService = AnswerService()
         answers: List[str] = answer_service.answer_questions(essay, queries)
+
+        self.logger.info("Finished processing POST request to /answers sucessfully.")
 
         return jsonify({"answers": answers})
